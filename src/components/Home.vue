731650fe1dfc71cf1de43992288c31fb8eeb8e12
andyleo <AndyLeo@tutanota.com>
2024-09-25 10:33:08 +0800

    Table display effect

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,47 +2,83 @@
   <section class="flex flex-col space-y-4">
     <span class="text-2xl font-semibold text-title">待确认交易</span>
     <div class="bg-card rounded-lg shadow-md p-4 text-text">
-      <div class="flex border-b border-gray-700 pb-2 mb-2">
+      <!-- <div class="flex border-b border-gray-700 pb-2 mb-2">
         <div class="w-1/6 font-semibold">记录ID</div>
         <div class="w-1/6 font-semibold">Token地址</div>
         <div class="w-1/6 font-semibold">转账合约地址</div>
         <div class="w-1/6 font-semibold">接收Token地址</div>
         <div class="w-1/6 font-semibold">提现金额</div>
         <div class="w-1/6 font-semibold">状态</div>
-      </div>
+      </div> -->
 
       <!-- <p class="flex">
-        <div class="w-1/6">{{ round }}</div>
+        <div class="w-1/6">{{ recordId }}</div>
         <div class="w-1/6">{{ prizePool }} ETH</div>
-        <div class="w-1/6">{{ players.length }}</div>
+        <div class="w-1/6">{{ levelTwoAddr.length }}</div>
         <div
-          :class="['w-1/6', status == 1 ? 'text-green-500' : 'text-red-500']"
+          :class="['w-1/6', state == 1 ? 'text-green-500' : 'text-red-500']"
         >
-          {{ status == 1 ? "RUNNING" : "NOT RUNNING" }}
+          {{ state == 1 ? "待确认" : "已确认" }}
         </div>
       </p> -->
-      {{ displayRecords }}
-      <div class="mt-4">
-        <p
-          id="helper-text-explanation"
-          class="text-sm text-gray-500 dark:text-gray-400"
-        >
-          Ticket fee should be 0.005 ether
-        </p>
-        <button
-          @click="buyTicket"
-          :disabled="loading || status != 1"
-          class="relative inline-flex items-center justify-center p-0.5 mb-2 mt-4 overflow-hidden text-sm font-medium text-title rounded-lg group bg-gradient-to-br from-btn to-btn1 group-hover:from-btn group-hover:to-btn1 hover:text-white focus:outline-none"
-        >
+      <el-table :data="tableData" row-key="id">
+        <el-table-column prop="recordId" label="记录ID" width="150px" />
+        <!-- <el-table-column type="expand">
+          <template #default="props">
+            <div class="p-[10px]" style="box-sizing: border-box;" v-if="props.row.list?.length">
+              详情
+              <el-table :data="props.row.list" :size="'small'">
+                <el-table-column prop="recordId" width="100px" label="记录ID" />
+                <el-table-column prop="tokenAddr" width="100px" label="Token地址" />
+                <el-table-column prop="levelTwoAddr" width="100px" label="转账合约地址" />
+                <el-table-column prop="receiver" width="100px" label="接收Token地址" />
+                <el-table-column prop="operation" width="100px" label="操作">
+                  <template #default="{ row }">
+                    <el-button type="text" size="small" @click="handleEdit(row)">Edit</el-button>
+                  </template>
+                </el-table-column>
+              </el-table>
+            </div>
+          </template>
+        </el-table-column> -->
+        <el-table-column prop="tokenAddr" label="Token地址" />
+        <el-table-column prop="levelTwoAddr" label="转账合约地址" />
+        <el-table-column prop="receiver" label="接收Token地址" />
+        <el-table-column prop="amount" label="提现金额" />
+        <el-table-column prop="state" label="状态">
+          <template #default="{ row }">
+            <span :class="[row.state == 0 ? 'text-green-500' : 'text-red-500']">
+              {{ row.state == 0 ? "待确认" : "已确认" }}
+            </span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="operation" width="100px" label="操作">
+          <template #default="{ row }">
+            <!-- <el-button type="button" size="small" @click="handleConfirmTransaction(row)">交易确认</el-button> -->
+            <div class="mt-4">
+              <button @click="handleConfirmTransaction(row)" :disabled="loading || row.state != 0">
+                <span
+                  class="relative px-1 py-1 transition-all ease-in duration-55 bg-card rounded-md group-hover:bg-opacity-0 cursor-pointer">
+                  交易确认
+                </span>
+              </button>
+              <div v-if="loading" class="text-sm text-gray-500">Processing...</div>
+            </div>
+          </template>
+        </el-table-column>
+      </el-table>
+      <!-- <div class="mt-4">
+        <button @click="buyTicket" :disabled="loading || state != 1"
+          class="relative inline-flex items-center justify-center p-0.5 mb-2 mt-4 overflow-hidden text-sm font-medium text-title rounded-lg group bg-gradient-to-br from-btn to-btn1 group-hover:from-btn group-hover:to-btn1 hover:text-white focus:outline-none">
           <span
-            class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-card rounded-md group-hover:bg-opacity-0 cursor-pointer"
-          >
+            class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-card rounded-md group-hover:bg-opacity-0 cursor-pointer">
             Buy Ticket
           </span>
         </button>
         <div v-if="loading" class="text-sm text-gray-500">Processing...</div>
-      </div>
+      </div> -->
     </div>
+
   </section>
 </template>
 
@@ -51,26 +87,59 @@ import { ref, onMounted } from "vue";
 import { toast } from "vue3-toastify";
 import { useWalletStore } from "../stores/walletStore";
 
-
 const walletStore = useWalletStore();
-const round = ref(0);
+const recordId = ref(0);
 const prizePool = ref(0);
-const status = ref("");
-const players = ref([]);
+const state = ref("");
+const levelTwoAddr = ref([]);
 const loading = ref(false);
+const tableData = ref([]);
 
-// 数据列表
-// const dataList;
 
 const fetchContractData = async () => {
   loading.value = true;
   try {
-    round.value = await walletStore.contract.methods.currentRound().call();
-    const prize = await walletStore.contract.methods.getPrizePool().call();
-    prizePool.value = walletStore.provider.utils.fromWei(prize, "ether");
-    players.value = await walletStore.contract.methods.getPlayers().call();
-    status.value = await walletStore.contract.methods.status().call();
-    // 获取数据列表
+    recordId.value = await walletStore.contract.methods.currentRound().call();
+    console.log(' recordId.value', recordId.value);
+    const tokenAddr = await walletStore.contract.methods.getPrizePool().call();
+    console.log(' tokenAddr.value', tokenAddr.value);
+    prizePool.value = walletStore.provider.utils.fromWei(tokenAddr, "ether");
+    console.log(' prizePool.value', prizePool.value);
+    levelTwoAddr.value = await walletStore.contract.methods.getPlayers().call();
+    console.log(' levelTwoAddr.value', levelTwoAddr.value);
+    state.value = await walletStore.contract.methods.status().call();
+    console.log(' state.value', state.value);
+    // tableData.value = [{
+    //   id: 3,
+    //   recordId: recordId.value,
+    //   tokenAddr: prizePool.value,
+    //   levelTwoAddr: levelTwoAddr.value.length,
+    //   state: state.value,
+    //   list: [{
+    //     id: 31,
+    //     recordId: recordId.value,
+    //     tokenAddr: prizePool.value,
+    //     levelTwoAddr: levelTwoAddr.value.length,
+    //     state: state.value,
+    //   }]
+    // }];
+
+
+    tableData.value = [{
+      id: 3,
+      recordId: recordId.value,
+      tokenAddr: prizePool.value,
+      levelTwoAddr: levelTwoAddr.value.length,
+      state: state.value,
+    },{
+      id: 4,
+      recordId: 4,
+      tokenAddr: prizePool.value,
+      levelTwoAddr: levelTwoAddr.value.length,
+      state: state.value,
+    }];
+    console.log(' tableData.value', tableData.value);
+
 
   } catch (error) {
     toast.error(`Failed to fetch contract data: ${error.message}`);
@@ -105,7 +174,52 @@ const buyTicket = async () => {
   }
 };
 
+
+const handleConfirmTransaction = (row) => {
+  console.log("row info:", row);
+  console.log("id:", row.id);
+  console.log("recordId:", row.recordId);
+  if(row.state != 0) {
+    alert("交易已完成, 无须确认!");
+  }
+  // alert("hello:" + row.id);
+};
 onMounted(() => {
   fetchContractData();
 });
 </script>
+<style>
+.el-table {
+  padding: 0;
+}
+
+.el-table th.el-table__cell {
+  background-color: #1e1e1e;
+}
+
+.el-table tr {
+  background-color: #1e1e1e;
+  color: #fff;
+}
+
+.el-table--fit {
+  background-color: transparent;
+}
+
+.el-table tr:hover {
+  background-color: #1e1e1e;
+}
+
+.el-table__empty-block {
+  background-color: #1e1e1e;
+}
+
+.el-table__expanded-cell {
+  background-color: #1e1e1e;
+}
+
+.el-table--enable-row-hover .el-table__body tr:hover>td.el-table__cell {
+  background-color: #1e1e1e;
+
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,30 +1,11 @@
 import { createApp } from "vue";
-import "./style.css";
+import ElementPlus from 'element-plus'
+
 import { createPinia } from "pinia";
 import Vue3Toastify from "vue3-toastify";
+import "./style.css";
+import 'element-plus/dist/index.css'
+
 import App from "./App.vue";
 const pinia = createPinia();
 createApp(App).use(pinia).use(Vue3Toastify, { autoClose: 3000 }).mount("#app");
-
-
-// import elTableSticky from '@cell-x/el-table-sticky'
-// createApp(App).use(elTableSticky, {
-//   StickyHeader: {
-//     // 吸顶偏移量，可以是 CSS 支持的距离值，如 `0px`、`10%`、`calc(100vh - 1rem)` 等
-//     offsetTop: 0,
-//     // 滚动条吸底偏移量，可以是 CSS 支持的距离值，如 `0px`、`10%`、`calc(100vh - 1rem)` 等
-//     offsetBottom: 0,
-//   },
-//   StickyFooter: {
-//     // 吸底偏移量，可以是 CSS 支持的距离值，如 `0px`、`10%`、`calc(100vh - 1rem)` 等
-//     offsetBottom: 0,
-//   },
-//   StickyScroller: {
-//     // 吸底偏移量，可以是 CSS 支持的距离值，如 `0px`、`10%`、`calc(100vh - 1rem)` 等
-//     offsetBottom: 0,
-//   },
-//   HeightAdaptive: {
-//     // 底部偏移量，只能是数字型
-//     offsetBottom: 0,
-//   }
-// })
